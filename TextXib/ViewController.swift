877318f813d7239d5ca4c650bd8b902ece0a5367
subhadip adhikary <subhadip.adhikary@zaggle.in>
2022-09-22 14:29:45 +0530

    Initial Commit

---
+++ TextXib/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextXib
+//
+//  Created by Zaggle on 22/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
